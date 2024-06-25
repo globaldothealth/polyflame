c98aab095a61f75eebbd43432ca1f8ed3b32a312
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2024-06-25 18:44:22 +0100

    Add docstrings

--- polyflame/types.py
+++ polyflame/types.py
@@ -1,3 +1,10 @@
+"""
+Types
+=====
+
+Types used throughout polyflame
+"""
+
 from pathlib import Path
 from typing import Literal, Required, TypedDict
 
@@ -8,29 +15,51 @@ Taxonomy = dict[str, dict[str, str | bool]]
 
 
 class ReadableTermColumnInfo(TypedDict, total=False):
+    """Dictionary type defining the mapping from term columns to
+    readable names in a taxonomy"""
+
     term_column: Required[str]
+    "Column in data that should be mapped from codes to readable names"
     taxonomy_section: str
+    """Section in taxonomy file that should be used for mapping.
+    If not given, this is taken to be the same as ``term_column``"""
     drop_nulls: bool
+    """Whether rows containing nulls (after mapping) should be dropped
+    from the data, by default, rows are not dropped"""
 
 
 class SourceInfo(TypedDict):
+    "Source information for analysis functions to load data"
     N: int | None
+    "Number of patients"
     path: Path
+    "Path where source is located"
     checksum: str
+    "Data integrity checksum"
     checksum_file: str
+    "File for which data integrity checksum is calculated"
 
 
 class PlotInfo(TypedDict, total=False):
-    """TypedDict passed to plotting functions"""
+    """Dictionary passed to plotting functions"""
 
     title: str
+    "Title of plot"
     cols: dict[str, str]
+    "Column mappings from the column types expected to the actual column names in data"
     colors: list[str]
+    "Color palette to use, must be a list of valid RGB color hex codes, with an appropriate length"
     colorSequential: str
+    "Palette name to use for sequential colors"
     xlabel: str
+    "Label for x-axis"
     ylabel: str
+    "Label for y-axis"
 
 
 class DataPlotInfo(PlotInfo):
+    "Extension of :py:class:`polyflame.types.PlotInfo` to add data and plot type"
     data: pd.DataFrame
+    "Data corresponding to the plot"
     type: PlotType | None
+    "Type of plot"
