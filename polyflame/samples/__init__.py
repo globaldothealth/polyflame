ad8b254628f0b69e7f0872c5a9a2384e99cdd386
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2024-06-26 11:01:14 +0100

    Store checksum_fhirflat for easier demos

--- polyflame/samples/__init__.py
+++ polyflame/samples/__init__.py
@@ -8,3 +8,4 @@ Sample data distributed with polyflame
 from pathlib import Path
 
 fhirflat = Path(__file__).parent / "sample-fhirflat"
+checksum_fhirflat = "03cc8e28d97a6a3ab20926d7c3f891f14e119eb882c6e8d3deb07e1b79eed089"
